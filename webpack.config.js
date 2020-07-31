5fc75c0d3875f3065e7ab7189a3126554ed5e87a
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-07-31 15:41:17 +0300

    updated: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,10 @@ const {join: joinPaths, resolve: resolvePath} = require('path');
 const SRC_DIRNAME = joinPaths(__dirname, 'src');
 const currentMode = (isDev) => isDev ? 'development' : 'production';
 const outputDirname = (isDev) => isDev ? 'dist' : 'public';
+const currentDevtool = (isDev) => isDev ? 'eval-source-map' : false;
 
 /** webpack plugins */
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 /** webpack config */
@@ -16,6 +18,8 @@ const webpackConfig = (env = {}) => {
   const isDev = env.isDev || false;
   return {
     mode: currentMode(isDev),
+    watch: isDev,
+    devtool: currentDevtool(isDev),
     entry: joinPaths(SRC_DIRNAME, 'index.jsx'),
     output: {
       path: resolvePath(__dirname, outputDirname(isDev)),
@@ -57,7 +61,16 @@ const webpackConfig = (env = {}) => {
               loader: 'css-loader',
               options: {
                 url: false,
+                importLoaders: 1,
                 sourceMap: isDev
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  require('postcss-short')({ prefix: 'x', skip: 'x' })
+                ]
               }
             },
             {
@@ -84,6 +97,7 @@ const webpackConfig = (env = {}) => {
       }
     },
     plugins: [
+      new CleanWebpackPlugin(),
       new MiniCssExtractPlugin({
         filename: 'css/planner.css'
       })
