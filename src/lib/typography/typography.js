6ce88e0cacce2e04adcd3ec170be76720115106f
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-02 00:05:31 +0300

    created: typography components object

---
+++ src/lib/typography/typography.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/** imports */
+import Heading from './heading';
+import Paragraph from './paragraph';
+
+/** Typography components */
+const Typography = {
+  Heading,
+  Paragraph,
+};
+
+/** export */
+export default Typography;
