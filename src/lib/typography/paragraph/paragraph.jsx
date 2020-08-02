ff2cd8c90e56546acfdc6f2eb3bcb34b04f79d5b
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-02 13:14:37 +0300

    updated: paragraph component

--- src/lib/typography/paragraph/paragraph.jsx
+++ src/lib/typography/paragraph/paragraph.jsx
@@ -9,6 +9,6 @@ export default function Paragraph(props) {
   let paragraphClassName = 'paragraph' + props.type;
   paragraphClassName += props.isBold ? '-bold' : '';
   return (
-    <p className={paragraphClassName}>{props.text}</p>
+    <div className={paragraphClassName}>{props.text}</div>
   )
 }
