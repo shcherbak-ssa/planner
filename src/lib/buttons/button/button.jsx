163c6a7b5580e992e9e03bbed3ac5bdb556a04b2
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-02 13:11:20 +0300

    draft: button component

---
+++ src/lib/buttons/button/button.jsx
@@ -0,0 +1,25 @@
+'use strict';
+
+/** imports */
+import React from 'react';
+import './button.scss';
+
+/** Button component */
+export default function Button(props) {
+  const dataButtonColor = props.color || 'primary';
+  const buttonClassName = props.isCircle ? 'button-circle' : 'button';
+  const buttonValue = getButtonValue(props);
+  return (
+    <div
+      className={buttonClassName}
+      data-class="mbr bs fc click"
+      data-button-color={dataButtonColor}
+    >{buttonValue}</div>
+  )
+}
+
+/** help functions */
+function getButtonValue(props) {
+  if (props.isCircle) return props.icon;
+  
+}
---
+++ src/lib/buttons/button/test-props.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/** test props */
+const testProps = {
+  isCircle: true,
+  icon: '+'
+};
+
+/** exports */
+export default testProps;
