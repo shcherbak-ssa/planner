6e895894e3a6e52ca3e76fef22601d2f27a03971
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-03 19:54:37 +0300

    draft: registration component

--- src/components/auth/registration/registration.jsx
+++ src/components/auth/registration/registration.jsx
@@ -2,6 +2,12 @@
 
 /** imports */
 import React from 'react';
+import {
+  Switch,
+  Route,
+  Redirect,
+  useRouteMatch,
+} from 'react-router-dom';
 
 // components
 import Frame from '../frame';
@@ -13,12 +19,26 @@ const CURRENT_MODE = 'registration';
 /** Registration component */
 export default function Registration(props) {
   /** data */
+  const {path} = useRouteMatch();
   const framebar = <GoogleAuth currentMode={CURRENT_MODE}/>;
 
   /** render */
   return (
-    <Frame heading="Registration" currentMode={CURRENT_MODE} framebar={framebar}>
-      Registration
-    </Frame>
+    <Switch>
+      <Route exact path={path}>
+        <Redirect to={`${path}/email`}/>
+      </Route>
+      <Route path={`${path}/email`}>
+        <Frame heading="Registration" currentMode={CURRENT_MODE} framebar={framebar}>
+          Registration
+        </Frame>
+      </Route>
+      <Route path={`${path}/code`}>
+        
+      </Route>
+      <Route path={`${path}/finish`}>
+        
+      </Route>
+    </Switch>
   )
 }
