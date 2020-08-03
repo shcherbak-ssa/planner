d48ced960bda772f87ba91253a19ec2475e91392
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-03 18:08:04 +0300

    draft: registration component

--- src/components/auth/registration/registration.jsx
+++ src/components/auth/registration/registration.jsx
@@ -3,10 +3,15 @@
 /** imports */
 import React from 'react';
 
+// components
+import Frame from '../frame';
+
 /** Registration component */
 export default function Registration(props) {
   /** render */
   return (
-    <h1>Registration component</h1>
+    <Frame>
+      Registration
+    </Frame>
   )
 }
