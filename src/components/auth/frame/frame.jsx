b6914b27c27b60818a1557bb8ee467c2113e3da5
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-03 18:41:30 +0300

    draft: frame component

--- src/components/auth/frame/frame.jsx
+++ src/components/auth/frame/frame.jsx
@@ -6,14 +6,26 @@ import './frame.scss';
 
 // components
 import Typography from '@lib/typography';
+import ChangeModeButton from './src/change-mode-button';
+
+/** constants */
+const modeLabels = {
+  login: 'Do not have an account?',
+  registration: 'Already have an account?'
+};
 
 /** Frame component */
 export default function Frame(props) {
+  /** data */
+  const {mode} = props;
+  const changeModeButtonLabel = modeLabels[mode];
+
   /** render */
   return (
     <div className="frame" data-class="bs bsh fbr pc white">
       <Typography.Heading type="4" name="frame-heading">{props.heading}</Typography.Heading>
       {props.children}
+      <ChangeModeButton label={changeModeButtonLabel} mode={`/${mode}`}/>
     </div>
   )
 }
