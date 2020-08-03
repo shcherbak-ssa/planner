4469e63fd44ed50eb79d36badc245157f50b2ca9
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-03 18:14:25 +0300

    draft: frame component

--- src/components/auth/frame/frame.jsx
+++ src/components/auth/frame/frame.jsx
@@ -4,11 +4,15 @@
 import React from 'react';
 import './frame.scss';
 
+// components
+import Typography from '@lib/typography';
+
 /** Frame component */
 export default function Frame(props) {
   /** render */
   return (
     <div className="frame" data-class="bs bsh fbr pc white">
+      <Typography.Heading type="4" name="frame-heading">{props.heading}</Typography.Heading>
       {props.children}
     </div>
   )
