54db7da81520db747440f3b345007f27ac763010
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-03 10:23:07 +0300

    draft: login component

---
+++ src/components/login/login.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+/** imports */
+import React from 'react';
+
+// components
+import Container from './container';
+
+/** Login component */
+export default function Login(props) {
+  /** render */
+  return (
+    <Container type={props.type}/>
+  )
+}
