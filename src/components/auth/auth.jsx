4cbad54ca91f803ab18b5b704dde85e6b59faee0
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-03 10:31:18 +0300

    draft: auth component

--- src/components/auth/auth.jsx
+++ src/components/auth/auth.jsx
@@ -4,12 +4,15 @@
 import React from 'react';
 
 // components
-import Container from './container';
+import Login from './login';
+import Registration from './registration';
 
 /** Auth component */
 export default function Auth(props) {
+  
   /** render */
-  return (
-    <Container type={props.type}/>
-  )
+  switch(props.mode) {
+    case 'login': return <Login/>
+    case 'registration': return <Registration/>
+  }
 }
