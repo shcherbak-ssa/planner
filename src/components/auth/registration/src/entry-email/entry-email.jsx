81875d8db631e0f51f7dfa8035513d3f40828bad
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-03 20:38:22 +0300

    draft: entry-email component

---
+++ src/components/auth/registration/src/entry-email/entry-email.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+/** imports */
+import React from 'react';
+
+// components
+import RegistrationFrame from '../registration-frame';
+
+/** EntryEmail component */
+export default function EntryEmail(props) {
+  /** render */
+  return (
+    <RegistrationFrame type="email"></RegistrationFrame>
+  )
+}
