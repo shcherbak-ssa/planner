e64be1fb23ce91bea06cba9fad2ad6f327617ba1
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-03 10:44:39 +0300

    draft: auth component

--- src/components/auth/auth.jsx
+++ src/components/auth/auth.jsx
@@ -1,18 +1,28 @@
 'use strict';
 
 /** imports */
-import React from 'react';
-
-// components
-import Login from './login';
-import Registration from './registration';
+import React, {useState, useEffect} from 'react';
+import importModule from '@module/tools/import-module';
 
 /** Auth component */
 export default function Auth(props) {
-  
-  /** render */
-  switch(props.mode) {
-    case 'login': return <Login/>
-    case 'registration': return <Registration/>
+  /** states */
+  const [mode, setMode] = useState(props.mode);
+
+  /** data */
+  let AuthComponent = '';
+
+  /** effect */
+  useEffect(async () => {
+    const authComponentPath = createAuthComponentPath();
+    AuthComponent = await importModule(authComponentPath);
+  });
+
+  /** methods */
+  function createAuthComponentPath() {
+    return `./${mode}`;
   }
+
+  /** render */
+  return <AuthComponent/>
 }
