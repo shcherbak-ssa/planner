17e37f406706e9becc2f2f112271966d7b1969f0
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-05 15:15:26 +0300

    created: user-local-storage module

---
+++ src/modules/user-local-storage.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+/** constants */
+const USER_DATA_STORAGE_KEY: string = 'user';
+
+/** UserLocalStorage */
+const UserLocalStorage: object = {
+  isUserExist() {
+    return !!getUserDataItem();
+  },
+  getUserData() {
+    return getUserDataItem();
+  },
+  saveUserData(email: string, password: string) {
+    const userData: string = transformToJSON(email, password);
+    localStorage.setItem(USER_DATA_STORAGE_KEY, userData);
+  }
+};
+
+/** src */
+function getUserDataItem() {
+  return localStorage.getItem(USER_DATA_STORAGE_KEY);
+}
+function transformToJSON(email: string, password: string) {
+  return JSON.stringify({email, password});
+}
+
+/** export */
+export default UserLocalStorage;
