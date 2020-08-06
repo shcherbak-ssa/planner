5d538a5a5247c00056b756b21c0a52d4cd174d95
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-06 21:46:55 +0300

    created: constants module of page app component

---
+++ src/components/app/components/page/constants.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/** constants */
+const DROPDOWN_TYPE_PROFILE = 'profile';
+const DROPDOWN_TYPE_SETTINGS = 'settings';
+const DROPDOWN_TYPE_LOGOUT = 'logout';
+
+/** export */
+export {
+  DROPDOWN_TYPE_PROFILE,
+  DROPDOWN_TYPE_SETTINGS,
+  DROPDOWN_TYPE_LOGOUT,
+}
--- src/components/app/components/page/dropdown-items.js
+++ src/components/app/components/page/dropdown-items.js
@@ -2,21 +2,26 @@
 
 /** imports */
 import {faUser, faCog, faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
+import {
+  DROPDOWN_TYPE_PROFILE,
+  DROPDOWN_TYPE_SETTINGS,
+  DROPDOWN_TYPE_LOGOUT,
+} from './constants';
 
 /** dropdownItems */
 const dropdownItems = [
   {
-    type: 'profile',
+    type: DROPDOWN_TYPE_PROFILE,
     text: 'Profile',
     icon: faUser
   },
   {
-    type: 'settings',
+    type: DROPDOWN_TYPE_SETTINGS,
     text: 'Settings',
     icon: faCog
   },
   {
-    type: 'log-out',
+    type: DROPDOWN_TYPE_LOGOUT,
     text: 'Log out',
     icon: faSignOutAlt
   }
