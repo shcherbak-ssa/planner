b1975549cebe31883d961ed5e4e07518f946245c
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-06 19:55:16 +0300

    created: constants app module

---
+++ src/components/app/constants.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/** constants */
+const ROOT_PATH = '/';
+const PROJECT_LIST_PATH = '/projects';
+const PROJECT_PATH = '/projects/:project';
+
+/** export */
+export {
+  ROOT_PATH,
+  PROJECT_LIST_PATH,
+  PROJECT_PATH,
+}
