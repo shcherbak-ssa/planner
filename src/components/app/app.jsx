901268d9a9e264e147af306f405473e3358fa120
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-06 20:21:17 +0300

    updated: app component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,22 +1,40 @@
 'use strict';
 
 /** imports */
-import React from 'react';
+import React, {useState} from 'react';
+import classnames from 'classnames';
 import DropdownService from '@service/dropdown';
+
+// assets
+import './app.scss';
 
 // components
 import Container from './components/container';
-import AppRouter from './app-router';
+import PageRouter from './pages/page-router';
 
 /** init */
 DropdownService.init();
 
 /** App component */
 export default function App(props) {
+  /** states */
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  /** data */
+  const appPopupClassName = classnames({
+    'app-popup': true,
+    'is-open': isPopupOpen
+  });
+
   /** render */
   return (
-    <Container>
-      <AppRouter/>
-    </Container>
+    <div className="app" data-class="full">
+      <div className="app-page" data-class="full">
+        <Container>
+          <PageRouter/>
+        </Container>
+      </div>
+      <div className={appPopupClassName}></div>
+    </div>
   )
 }
