a914dc9b7e66e12c4c5e38b0ccdac034ea8da6a3
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-06 18:30:26 +0300

    created: frame-title app component

--- src/components/app/components/frame-title/frame-title.jsx
+++ src/components/app/components/frame-title/frame-title.jsx
@@ -2,8 +2,30 @@
 
 /** imports */
 import React from 'react';
+import {useLocation} from 'react-router-dom';
+
+// assets
+import './frame-title.scss';
+
+// components
+import Typography from '@lib/typography';
 
 /** FrameTitle component */
 export default function FrameTitle(props) {
+  /** states */
+  const location = useLocation();
+
+  /** methods */
+  function setTitle() {
+    const pathname = location.pathname.split('/');
+    pathname[0] = 'Planner';
+    return pathname.join(' / ');
+  }
+
   /** render */
+  return (
+    <div className="frame-title">
+      <Typography.Heading type="6">{setTitle()}</Typography.Heading>
+    </div>
+  )
 }
