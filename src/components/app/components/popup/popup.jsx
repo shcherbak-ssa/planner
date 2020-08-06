394fd1eaae0066f830b7092e32ae13ba1aa44471
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-06 21:28:21 +0300

    draft: popup app component

--- src/components/app/components/popup/popup.jsx
+++ src/components/app/components/popup/popup.jsx
@@ -2,19 +2,32 @@
 
 /** imports */
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 import './popup.scss';
 
 // components
 import FrameTitle from '../frame-title';
+import ClosePopupButton from '../close-popup-button';
 
 /** Popup component */
 export default function Popup(props) {
+  /** states */
+  const history = useHistory();
+
+  /** methods */
+  function closePopupButtonClickHandler(e) {
+    e.preventDefault();
+    history.goBack();
+  }
+
   /** render */
   return (
     <div className="popup" data-class="bs full">
       <div className="popup-header" data-class="fsb">
         <FrameTitle/>
+        <ClosePopupButton clickHandler={closePopupButtonClickHandler}/>
       </div>
+      <div className="popup-content"></div>
     </div>
   )
 }
