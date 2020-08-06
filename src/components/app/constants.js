a63d42b3097328413c393d5b332f23d4e36bd40e
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-06 23:39:47 +0300

    added: new constants to app constants module

--- src/components/app/constants.js
+++ src/components/app/constants.js
@@ -1,18 +1,32 @@
 'use strict';
 
 /** constants */
-const ROOT_PATH = '/';
-const PROJECT_LIST_PATH = '/projects';
-const PROJECT_PATH = '/projects/:project';
+const PAGE_HOME_PATH = '/';
+const PAGE_PROJECT_LIST_PATH = '/projects';
+const PAGE_PROJECT_DETAILS_PATH = '/:project/details';
+const PAGE_PROJECT_TASKS_PATH = '/:project/tasks';
+const PAGE_PROJECT_TASK_OPEN_PATH = '/:project/:task';
 
 const POPUP_PROFILE_PATH = '/profile';
 const POPUP_SETTINGS_PATH = '/settings';
+const POPUP_CREATE_PROJECT_PATH = '/create-project';
+const POPUP_DELETE_PROJECT_PATH = '/:project/delete';
+const POPUP_PROJECT_SETTINGS_PATH = '/:project/settings';
+const POPUP_PROJECT_GOAL_PATH = '/:project/:goal';
+const POPUP_PROJECT_TASK_PATH = '/:project/:task';
 
 /** export */
 export {
-  ROOT_PATH,
-  PROJECT_LIST_PATH,
-  PROJECT_PATH,
+  PAGE_HOME_PATH,
+  PAGE_PROJECT_LIST_PATH,
+  PAGE_PROJECT_DETAILS_PATH,
+  PAGE_PROJECT_TASKS_PATH,
+  PAGE_PROJECT_TASK_OPEN_PATH,
   POPUP_PROFILE_PATH,
   POPUP_SETTINGS_PATH,
+  POPUP_CREATE_PROJECT_PATH,
+  POPUP_DELETE_PROJECT_PATH,
+  POPUP_PROJECT_SETTINGS_PATH,
+  POPUP_PROJECT_GOAL_PATH,
+  POPUP_PROJECT_TASK_PATH
 }
