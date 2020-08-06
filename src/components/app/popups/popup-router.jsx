7be180aa06a80d44f6a473af85b8d03599ceb1c3
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-06 23:44:57 +0300

    created: popup-router component

---
+++ src/components/app/popups/popup-router.jsx
@@ -0,0 +1,30 @@
+'use strict';
+
+/** imports */
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import {
+  POPUP_PROFILE_PATH,
+  POPUP_SETTINGS_PATH,
+  POPUP_CREATE_PROJECT_PATH,
+  POPUP_DELETE_PROJECT_PATH,
+  POPUP_PROJECT_SETTINGS_PATH,
+  POPUP_PROJECT_GOAL_PATH,
+  POPUP_PROJECT_TASK_PATH
+} from '../constants';
+
+/** PopupRouter component */
+export default function PopupRouter(props) {
+  /** render */
+  return (
+    <Switch>
+      <Route path={POPUP_PROFILE_PATH}></Route>
+      <Route path={POPUP_SETTINGS_PATH}></Route>
+      <Route path={POPUP_CREATE_PROJECT_PATH}></Route>
+      <Route path={POPUP_DELETE_PROJECT_PATH}></Route>
+      <Route path={POPUP_PROJECT_SETTINGS_PATH}></Route>
+      <Route path={POPUP_PROJECT_GOAL_PATH}></Route>
+      <Route path={POPUP_PROJECT_TASK_PATH}></Route>
+    </Switch>
+  )
+}
