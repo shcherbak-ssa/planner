7e5ed8127632be3a7fff353a4331931e22c60248
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-06 20:26:55 +0300

    draft: popup app component

---
+++ src/components/app/components/popup/popup.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+/** imports */
+import React from 'react';
+import './popup.scss';
+
+/** Popup component */
+export default function Popup(props) {
+  /** render */
+  return (
+    <div className="popup" data-class="full"></div>
+  )
+}
