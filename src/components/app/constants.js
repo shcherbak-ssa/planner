63d14c2756aba886493bec367aecfd0213256110
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-06 21:43:01 +0300

    added: popup paths in constants app module

--- src/components/app/constants.js
+++ src/components/app/constants.js
@@ -5,9 +5,14 @@ const ROOT_PATH = '/';
 const PROJECT_LIST_PATH = '/projects';
 const PROJECT_PATH = '/projects/:project';
 
+const POPUP_PROFILE_PATH = '/profile';
+const POPUP_SETTINGS_PATH = '/settings';
+
 /** export */
 export {
   ROOT_PATH,
   PROJECT_LIST_PATH,
   PROJECT_PATH,
+  POPUP_PROFILE_PATH,
+  POPUP_SETTINGS_PATH,
 }
