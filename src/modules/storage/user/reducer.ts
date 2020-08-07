658882065275849324c56681cb363d57d77461aa
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-08 01:14:31 +0300

    draft: reducer user storage module

---
+++ src/modules/storage/user/reducer.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+/** imports */
+import UserData from '../../user/data';
+
+/** userStorageReducer */
+function userStorageReducer(state: UserData, action: UserStorageAction): UserData {}
+
+/** export */
+export default userStorageReducer;
