45d9cde9ba09fc4253d4939c7ec18a1afb654eb8
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-08 00:27:09 +0300

    draft: user-save user module

--- src/modules/user/user-save.ts
+++ src/modules/user/user-save.ts
@@ -1,10 +1,24 @@
 'use strict';
 
+/** imports */
+import UserData from './user-data';
+import UserStorageState from './user-storage-state';
+import Storage from '../storage/user';
+
+/** init */
+const userStorageState: UserStorageState = new Storage.UserState();
+
 /** UserSave */
 class UserSave {
+  static lastState: UserData;
+
   /** static methods */
-  static saveCurrentState() {}
-  static async saveUpdatedDataOnServer() {}
+  static saveCurrentState() {
+    UserSave.lastState = userStorageState.getCurrentStorageState();
+  }
+  static async saveUpdatedDataOnServer() {
+    const currentState = userStorageState.getCurrentStorageState();
+  }
 }
 
 /** export */
