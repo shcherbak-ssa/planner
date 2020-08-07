0d9291778a7f9857a638e8b1cb90d4fbd3fcea80
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-07 23:57:58 +0300

    draft: user-save user module

---
+++ src/modules/user/user-save.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+/** UserSave */
+class UserSave {
+  /** static methods */
+  static saveCurrentState() {}
+  static async saveUpdatedData() {}
+}
+
+/** export */
+export default UserSave;
