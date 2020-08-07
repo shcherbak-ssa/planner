855a650154501d4658a67f0545c465bf48f0af92
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-07 23:54:33 +0300

    created: user events module

---
+++ src/modules/events/user.ts
@@ -0,0 +1,43 @@
+'use strict';
+
+/** imports */
+import EventEmitter from './emitter';
+import UserDataUpdater from '../user/user-data-updater';
+
+/** constants */
+const USER_EDIT_INIT: string = 'user-edit-init';
+const USER_NAME_UPDATE: string = 'user-name-update';
+const USER_USERNAME_UPDATE: string = 'user-username-update';
+const USER_EMAIL_UPDATE: string = 'user-email-update';
+const USER_UPDATED_DATA_SAVE: string = 'user-updated-data-save';
+
+/** init */
+const userEventEmitter: EventEmitter = new EventEmitter();
+userEventEmitter.on(USER_EDIT_INIT, userEditInitHandler);
+
+function userEditInitHandler() {
+  const userDataUpdater: UserDataUpdater = new UserDataUpdater();
+  userEventEmitter
+    .on(USER_NAME_UPDATE, userDataUpdater.updateName)
+    .on(USER_USERNAME_UPDATE, userDataUpdater.updateUsername)
+    .on(USER_EMAIL_UPDATE, userDataUpdater.updateEmail)
+    .on(USER_UPDATED_DATA_SAVE, userUpdatedDataSave);
+
+  function userUpdatedDataSave() {
+    userEventEmitter
+      .off(USER_NAME_UPDATE, userDataUpdater.updateName)
+      .off(USER_USERNAME_UPDATE, userDataUpdater.updateUsername)
+      .off(USER_EMAIL_UPDATE, userDataUpdater.updateEmail)
+      .off(USER_UPDATED_DATA_SAVE, userUpdatedDataSave);
+  }
+}
+
+/** export */
+export {
+  userEventEmitter,
+  USER_EDIT_INIT,
+  USER_NAME_UPDATE,
+  USER_USERNAME_UPDATE,
+  USER_EMAIL_UPDATE,
+  USER_UPDATED_DATA_SAVE,
+}
