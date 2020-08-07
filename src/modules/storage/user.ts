3d02b00f472564727c72c176ad50401896e2e48e
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-08 00:13:12 +0300

    added: user-storage-state implementation to user storage module

--- src/modules/storage/user.ts
+++ src/modules/storage/user.ts
@@ -1,7 +1,9 @@
 'use strict';
 
 /** imports */
+import UserData from '../user/user-data';
 import UserStorage from '../user/user-storage';
+import UserStorageState from '../user/user-storage-state';
 
 /** User */
 class User implements UserStorage {
@@ -10,5 +12,16 @@ class User implements UserStorage {
   updateEmail(email: string): void {}
 }
 
+/** UserState */
+class UserState implements UserStorageState {
+  getCurrentStorageState(): UserData {}
+}
+
+/** Storage */
+const Storage = {
+  User,
+  UserState
+}
+
 /** export */
-export default User;
+export default Storage;
