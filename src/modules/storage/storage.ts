f0146748fef574aac63e08e05e48664512906420
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-08 02:10:37 +0300

    draft: storage module

--- src/modules/storage/storage.ts
+++ src/modules/storage/storage.ts
@@ -1,1 +1,16 @@
 'use strict';
+
+/** imports */
+import {createStore} from 'redux';
+
+// reducers
+import userStorageReducer from './user/reducer';
+
+/** Storage */
+const Storage = {
+  
+  init(callback: Function) {}
+};
+
+/** export */
+export default Storage;
