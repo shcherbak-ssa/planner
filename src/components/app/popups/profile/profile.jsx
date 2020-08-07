e7cd605c7362c661d9e6686e4183db25da372b80
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-07 15:51:49 +0300

    draft: profile popup app component

---
+++ src/components/app/popups/profile/profile.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+/** imports */
+import React from 'react';
+import './profile.scss';
+
+/** Profile component */
+export default function Profile(props) {
+  /** render */
+  return (
+    <div className="profile"></div>
+  )
+}
