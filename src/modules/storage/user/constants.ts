0e14ae2801f523a084947f8288ea29e4f6e0b363
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-08 01:12:21 +0300

    created: constants user storage module

---
+++ src/modules/storage/user/constants.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+/** constants */
+const UPDATE_NAME: string = 'UPDATE_NAME';
+const UPDATE_USERNAME: string = 'UPDATE_USERNAME';
+const UPDATE_EMAIL: string = 'UPDATE_EMAIL';
+const GET_CURRENT_STORAGE_STATE: string = 'GET_CURRENT_STORAGE_STATE';
+
+/** export */
+export {
+  UPDATE_NAME,
+  UPDATE_USERNAME,
+  UPDATE_EMAIL,
+  GET_CURRENT_STORAGE_STATE,
+}
