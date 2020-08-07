e3bf43c5a88b134032e2517508c0860dcfc185a6
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-07 18:01:43 +0300

    added: popup-container element logic to popup component

--- src/components/app/components/popup/popup.jsx
+++ src/components/app/components/popup/popup.jsx
@@ -1,7 +1,7 @@
 'use strict';
 
 /** imports */
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {useHistory} from 'react-router-dom';
 import './popup.scss';
 
@@ -9,10 +9,23 @@ import './popup.scss';
 import FrameTitle from '../frame-title';
 import ClosePopupButton from '../close-popup-button';
 
+/** constants */
+const POPUP_CONTAINER_TOP_POSITION = 120;
+
 /** Popup component */
 export default function Popup(props) {
   /** states */
   const history = useHistory();
+  const popupContainer = useRef(null);
+  const [popupContainerStyle, setPopupContainerStyle] = useState({});
+
+  /** data */
+  const popupContainerAttributes = {
+    className: 'popup-container',
+    'data-class': 'bs mbr pc white',
+    ref: popupContainer,
+    style: popupContainerStyle
+  };
 
   /** methods */
   function closePopupHandler(e) {
@@ -25,6 +38,24 @@ export default function Popup(props) {
     }
   }
 
+  /** effects */
+  useEffect(() => {
+    const containerHeight = popupContainer.offsetHeight;
+    const validContainerHeight = getValidContainerHeight();
+    if (containerHeight >= validContainerHeight) {
+      setPopupContainerStyle({
+        top: POPUP_CONTAINER_TOP_POSITION + 'px',
+        transform: 'translateX(-50%)',
+      })
+    }
+    
+    function getValidContainerHeight() {
+      const documentHeight = document.documentElement.clientHeight;
+      const popupContainerPadding = POPUP_CONTAINER_TOP_POSITION * 2;
+      return documentHeight - popupContainerPadding;
+    }
+  }, [props.children])
+
   /** render */
   return (
     <div className="popup" data-class="bs full" onClick={popupClickHandler}>
@@ -32,7 +63,7 @@ export default function Popup(props) {
         <FrameTitle/>
         <ClosePopupButton clickHandler={closePopupHandler}/>
       </div>
-      <div className="popup-container" data-class="bs mbr white">
+      <div {...popupContainerAttributes}>
         {props.children}
       </div>
     </div>
