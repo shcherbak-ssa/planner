46728f394d875f676de0c2d7ed561c2800347e7a
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-08 02:12:33 +0300

    created: states storage module

---
+++ src/modules/storage/states.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+/** imports */
+import UserData from '../user/data';
+
+/** StorageStates */
+interface StorageStates {
+  user: UserData;
+}
+
+/** export */
+export default StorageStates;
