93a034ac7c8cc315f3b8483c4f05a4e755674eb2
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-08 01:41:00 +0300

    draft: reducer user storage module

--- src/modules/storage/user/reducer.ts
+++ src/modules/storage/user/reducer.ts
@@ -2,9 +2,24 @@
 
 /** imports */
 import UserData from '../../user/data';
+import UserActionType from './action-type';
+
+/** init */
+const initialState: UserData = {
+  name: '',
+  username: '',
+  email: ''
+}
 
 /** userStorageReducer */
-function userStorageReducer(state: UserData, action: UserStorageAction): UserData {}
+function userStorageReducer(
+  state: UserData = initialState,
+  action: UserActionType
+): UserData {
+  switch(action.type) {
+    default: return state;
+  }
+}
 
 /** export */
 export default userStorageReducer;
