3823820e5c5f1715e500243c2cb28ead522f8b96
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-09 00:28:24 +0300

    created: data-save user module

---
+++ src/modules/user/data-save.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+/** UserDataSave */
+class UserDataSave {
+  saveName(name: string) {}
+  saveUsername(username: string) {}
+  saveEmail(email: string) {}
+}
+
+/** export */
+export default UserDataSave;
