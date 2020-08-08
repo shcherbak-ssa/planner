4d384bd262e77eed3ef8db4167f7c8e0514a4566
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-09 00:18:57 +0300

    added: save events to user events module

--- src/modules/events/user.ts
+++ src/modules/events/user.ts
@@ -3,14 +3,16 @@
 /** imports */
 import EventEmitter from './emitter';
 import UserDataUpdater from '../user/data-updater';
-import UserSave from '../user/save';
 
 /** constants */
 const USER_EDIT_INIT: string = 'user-edit-init';
+const USER_EDIT_SAVE: string = 'user-edit-save';
 const USER_NAME_UPDATE: string = 'user-name-update';
+const USER_NAME_SAVE: string = 'user-name-save';
 const USER_USERNAME_UPDATE: string = 'user-username-update';
+const USER_USERNAME_SAVE: string = 'user-username-save';
 const USER_EMAIL_UPDATE: string = 'user-email-update';
-const USER_UPDATED_DATA_SAVE: string = 'user-updated-data-save';
+const USER_EMAIL_SAVE: string = 'user-email-save';
 
 /** init */
 const userEventEmitter: EventEmitter = new EventEmitter();
@@ -23,20 +25,18 @@ const updateEmail = userDataUpdater.updateEmail.bind(userDataUpdater);
 userEventEmitter.on(USER_EDIT_INIT, userEditInitHandler);
 
 function userEditInitHandler() {
-  // UserSave.saveCurrentState();
   userEventEmitter
     .on(USER_NAME_UPDATE, updateName)
     .on(USER_USERNAME_UPDATE, updateUsername)
     .on(USER_EMAIL_UPDATE, updateEmail)
-    .on(USER_UPDATED_DATA_SAVE, userUpdatedDataSave);
+    .on(USER_EDIT_SAVE, userEditSaveHandler);
 
-  function userUpdatedDataSave() {
-    // UserSave.saveUpdatedDataOnServer();
+  function userEditSaveHandler() {
     userEventEmitter
       .off(USER_NAME_UPDATE, updateName)
       .off(USER_USERNAME_UPDATE, updateUsername)
       .off(USER_EMAIL_UPDATE, updateEmail)
-      .off(USER_UPDATED_DATA_SAVE, userUpdatedDataSave);
+      .off(USER_EDIT_SAVE, userEditSaveHandler);
   }
 }
 
@@ -44,8 +44,11 @@ function userEditInitHandler() {
 export {
   userEventEmitter,
   USER_EDIT_INIT,
+  USER_EDIT_SAVE,
   USER_NAME_UPDATE,
+  USER_NAME_SAVE,
   USER_USERNAME_UPDATE,
+  USER_USERNAME_SAVE,
   USER_EMAIL_UPDATE,
-  USER_UPDATED_DATA_SAVE,
+  USER_EMAIL_SAVE,
 }
