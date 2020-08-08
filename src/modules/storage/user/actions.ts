649d747b05407c66620a2355ad109e8cc73456ec
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-08 22:49:13 +0300

    refactored: actions user storage module

--- src/modules/storage/user/actions.ts
+++ src/modules/storage/user/actions.ts
@@ -2,35 +2,25 @@
 
 /** imports */
 import UserActionType from './action-type';
-import {
-  UPDATE_NAME,
-  UPDATE_USERNAME,
-  UPDATE_EMAIL,
-  GET_CURRENT_STORAGE_STATE,
-} from './constants';
+import Constants from './constants';
 
 /** actions */
 function updateName(name: string): UserActionType {
   return {
-    type: UPDATE_NAME,
+    type: Constants.UPDATE_NAME,
     name
   }
 }
 function updateUsername(username: string): UserActionType {
   return {
-    type: UPDATE_USERNAME,
+    type: Constants.UPDATE_USERNAME,
     username
   }
 }
 function updateEmail(email: string): UserActionType {
   return {
-    type: UPDATE_EMAIL,
-    name
-  }
-}
-function getCurrentStorageState(): UserActionType {
-  return {
-    type: GET_CURRENT_STORAGE_STATE
+    type: Constants.UPDATE_EMAIL,
+    email
   }
 }
 
@@ -39,5 +29,4 @@ export {
   updateName,
   updateUsername,
   updateEmail,
-  getCurrentStorageState,
 }
