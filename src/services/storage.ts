009fe7ea794524d6b45fe33a31d0de1a6c437721
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-08 21:24:46 +0300

    created: storage service

---
+++ src/services/storage.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+/** imports */
+import Storage from '../modules/storage';
+
+/** StorageService */
+const StorageService = {
+  init() {
+    return Storage.init();
+  },
+  
+};
+
+/** export */
+export default StorageService;
