8a7b6f1dd13ee572ff66e508a0d3ad9b4eb0ff81
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-08 23:20:51 +0300

    added: implementions to user storage module

--- src/modules/storage/user/user.ts
+++ src/modules/storage/user/user.ts
@@ -1,15 +1,27 @@
 'use strict';
 
 /** imports */
+import Storage from '../storage';
 import UserData from '../../user/data';
 import UserStorage from '../../user/storage';
 import UserStorageState from '../../user/storage-state';
+import {
+  updateName,
+  updateUsername,
+  updateEmail,
+} from './actions';
 
 /** User */
 class User implements UserStorage, UserStorageState {
-  updateName(name: string): void {}
-  updateUsername(username: string): void {}
-  updateEmail(email: string): void {}
+  updateName(name: string): void {
+    Storage.doAction(updateName(name));
+  }
+  updateUsername(username: string): void {
+    Storage.doAction(updateUsername(username));
+  }
+  updateEmail(email: string): void {
+    Storage.doAction(updateEmail(email));
+  }
   getCurrentStorageState(): UserData {
     return {
       name: '',
