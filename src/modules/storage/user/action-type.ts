ed19852611713cc927e04ced67907bb95262ef49
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-08 22:36:29 +0300

    updated: transform constants to enum in constants user storage module

--- src/modules/storage/user/action-type.ts
+++ src/modules/storage/user/action-type.ts
@@ -1,36 +1,31 @@
 'use strict';
 
 /** imports */
-import {
-  UPDATE_NAME,
-  UPDATE_USERNAME,
-  UPDATE_EMAIL,
-  GET_CURRENT_STORAGE_STATE
-} from './constants';
+import Constants from './constants';
 
 /** action types */
 interface UpdateNameAction {
-  type: typeof UPDATE_NAME;
+  type: typeof Constants.UPDATE_NAME;
   name: string;
 }
 interface UpdateUsernameAction {
-  type: typeof UPDATE_USERNAME;
+  type: typeof Constants.UPDATE_USERNAME;
   username: string;
 }
 interface UpdateEmailAction {
-  type: typeof UPDATE_EMAIL;
+  type: typeof Constants.UPDATE_EMAIL;
   email: string;
 }
 interface GetCurrentStorageState {
-  type: typeof GET_CURRENT_STORAGE_STATE;
+  type: typeof Constants.GET_CURRENT_STORAGE_STATE;
 }
 
 /** UserActionType */
 type UserActionType =
-  UpdateNameAction |
-  UpdateUsernameAction |
-  UpdateEmailAction |
-  GetCurrentStorageState;
+  | UpdateNameAction
+  | UpdateUsernameAction
+  | UpdateEmailAction
+  | GetCurrentStorageState;
 
 /** export */
 export default UserActionType;
