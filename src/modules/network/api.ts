108c0b88b7a5278fb986296ad93081dc71303974
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-19 16:08:54 +0300

    deleted: error network module && refactored: api network module

--- src/modules/network/api.ts
+++ src/modules/network/api.ts
@@ -5,23 +5,14 @@ import {
   parseFromJSON,
   transformToJSON,
 } from '../../tools/json';
-import { NetworkError } from './errors';
-
-/** constants */
-const getPathDoesNotExist = (path: string) => `${path} does not exist`;
-const createPathExist = (path: string) => `${path} already exist`;
 
 /** NetworkAPI */
 export const NetworkAPI = {
   async get(path: string) {
-    if (!isPathExist(path)) throw new NetworkError(getPathDoesNotExist(path));
-
     const getData: any = getLocalStorageItem(path);
     return parseFromJSON(getData);
   },
   async create(path: string, data: any) {
-    if (isPathExist(path)) throw new NetworkError(createPathExist(path));
-
     const createData: string = transformToJSON(data);
     setLocalStorageItem(path, createData);
   },
@@ -30,9 +21,6 @@ export const NetworkAPI = {
 };
 
 /** src */
-function isPathExist(path: string) {
-  return !!getLocalStorageItem(path);
-}
 function getLocalStorageItem(path: string) {
   return localStorage.getItem(path);
 }
